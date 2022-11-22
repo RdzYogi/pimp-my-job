f546f145bc03807951b6b36c37ca4dfbd92ed386
Silviu Rosu <silviurosu1988@gmail.com>
2022-11-22 12:13:27 +0100

    added better seeds for jobs and basic index format

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,12 @@ User.create!({ name: "John", email: "john@gmail.com", password: "123456" })
 User.create!({ name: "Paul", email: "paul@gmail.com", password: "123456" })
 User.create!({ name: "Jim", email: "jim@gmail.com", password: "123456" })
 
-
 user = User.first.id
 
 10.times do
-  Job.create!({ description: Faker::Company.bs, user_id: user, price: rand(100...1000).to_i })
+  Job.create!({ description: Faker::Company.bs,
+                name: Faker::Construction.subcontract_category,
+                user_id: user,
+                deadline: Faker::Date.forward(days: rand(20..200)),
+                price: rand(100...1000).to_i })
 end
