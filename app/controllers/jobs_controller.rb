fcecedf6fbd72f134195911cb34969142348d61e
Silviu Rosu <silviurosu1988@gmail.com>
2022-11-22 11:38:41 +0100

    finished basic index

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,5 +1,6 @@
 class JobsController < ApplicationController
   before_action :set_job, only: %i[show edit update destroy]
+  skip_before_action :authenticate_user!, only: :index
 
   def index
     @jobs = Job.all
