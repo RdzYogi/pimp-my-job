31c3949b95a6d633c8ec32660a000acc0b398ddf
Vito Lattanzio <vlattanzio13@gmail.com>
2022-11-23 12:37:03 +0100

    updated navbar

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,7 @@ Rails.application.routes.draw do
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-
+  get '/dashboard', to: 'jobs#dashboard'
   resources :jobs, only: %i[index show new create edit update destroy] do
     resources :offers, only: %i[new create]
   end
